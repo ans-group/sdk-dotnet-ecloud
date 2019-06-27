e9ec0f179f503a6f6a6573c366bbbcca831a0239
Lee Spottiswood <lee.spottiswood@ukfast.co.uk>
2019-06-27 10:54:23 +0100

    remove resource field

--- UKFast.API.Client.ECloud/Operations/ApplianceOperations.cs
+++ UKFast.API.Client.ECloud/Operations/ApplianceOperations.cs
@@ -8,10 +8,7 @@ namespace UKFast.API.Client.ECloud.Operations
 {
     public class ApplianceOperations<T> : ECloudOperations, IApplianceOperations<T> where T : Appliance
     {
-        public ApplianceOperations(IUKFastECloudClient client) : base(client)
-        {
-            this._resource = this._resource + "/v1/appliances";
-        }
+        public ApplianceOperations(IUKFastECloudClient client) : base(client) { }
 
         public async Task<IList<T>> GetAppliancesAsync(ClientRequestParameters parameters = null)
         {
@@ -20,7 +17,7 @@ namespace UKFast.API.Client.ECloud.Operations
 
         public async Task<Paginated<T>> GetAppliancesPaginatedAsync(ClientRequestParameters parameters = null)
         {
-            return await this.Client.GetPaginatedAsync<T>(_resource, parameters);
+            return await this.Client.GetPaginatedAsync<T>("/ecloud/v1/appliances", parameters);
         }
 
         public async Task<T> GetApplianceAsync(string applianceID)
@@ -30,7 +27,7 @@ namespace UKFast.API.Client.ECloud.Operations
                 throw new Client.Exception.UKFastClientValidationException("Invalid appliance id");
             }
 
-            return await this.Client.GetAsync<T>($"{_resource }/{applianceID}");
+            return await this.Client.GetAsync<T>($"/ecloud/v1/appliances/{applianceID}");
         }
     }
 }
--- UKFast.API.Client.ECloud/Operations/ECloudOperations.cs
+++ UKFast.API.Client.ECloud/Operations/ECloudOperations.cs
@@ -5,7 +5,5 @@ namespace UKFast.API.Client.ECloud.Operations
     public abstract class ECloudOperations : OperationsBase<IUKFastECloudClient>
     {
         public ECloudOperations(IUKFastECloudClient client) : base(client) { }
-
-        protected string _resource = "/ecloud";
     }
 }
