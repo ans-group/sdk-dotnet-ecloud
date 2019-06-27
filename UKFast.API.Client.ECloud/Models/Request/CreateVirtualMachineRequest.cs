b4df384dc44bb138ea0820ef5b1359bfb242e37d
Mark Johnson <mark.johnson@ukfast.co.uk>
2019-06-27 10:41:51 +0100

    CreateVirtualMachineRequest update nullvalue handling

--- UKFast.API.Client.ECloud/Models/Request/CreateVirtualMachineRequest.cs
+++ UKFast.API.Client.ECloud/Models/Request/CreateVirtualMachineRequest.cs
@@ -10,14 +10,14 @@ namespace UKFast.API.Client.ECloud.Models.Request
         [Newtonsoft.Json.JsonProperty("environment")]
         public string Environment { get; set; }
 
-        [Newtonsoft.Json.JsonProperty("template", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string Template { get; set; } = null;
+        [Newtonsoft.Json.JsonProperty("template", NullValueHandling = NullValueHandling.Ignore)]
+        public string Template { get; set; }
 
-        [Newtonsoft.Json.JsonProperty("template_password", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string TemplatePassword { get; set; } = null;
+        [Newtonsoft.Json.JsonProperty("template_password", NullValueHandling = NullValueHandling.Ignore)]
+        public string TemplatePassword { get; set; }
 
-        [Newtonsoft.Json.JsonProperty("appliance_id", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public string ApplianceID { get; set; } = null;
+        [Newtonsoft.Json.JsonProperty("appliance_id", NullValueHandling = NullValueHandling.Ignore)]
+        public string ApplianceID { get; set; }
 
         [Newtonsoft.Json.JsonProperty("role")]
         public string Role { get; set; }
@@ -32,7 +32,7 @@ namespace UKFast.API.Client.ECloud.Models.Request
         public int RAM { get; set; }
 
         [Newtonsoft.Json.JsonProperty("hdd", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int? HDD { get; set; } = null;
+        public int HDD { get; set; }
 
         [Newtonsoft.Json.JsonProperty("hdd_disks", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public IEnumerable<CreateVirtualMachineRequestDisk> HDDDisks { get; set; } = null;
@@ -40,44 +40,44 @@ namespace UKFast.API.Client.ECloud.Models.Request
         [Newtonsoft.Json.JsonProperty("name")]
         public string Name { get; set; }
 
-        [Newtonsoft.Json.JsonProperty("computername", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        [Newtonsoft.Json.JsonProperty("computername", NullValueHandling = NullValueHandling.Ignore)]
         public string ComputerName { get; set; } = null;
 
         [Newtonsoft.Json.JsonProperty("tags", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public IEnumerable<CreateTagRequest> Tags { get; set; } = null;
 
         [Newtonsoft.Json.JsonProperty("backup", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public bool? Backup { get; set; } = null;
+        public bool Backup { get; set; }
 
         [Newtonsoft.Json.JsonProperty("support", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public bool? Support { get; set; } = null;
+        public bool Support { get; set; }
 
         [Newtonsoft.Json.JsonProperty("monitoring", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public bool? Monitoring { get; set; } = null;
+        public bool Monitoring { get; set; }
 
         [Newtonsoft.Json.JsonProperty("monitoring_contacts", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public IEnumerable<int> MonitoringContacts { get; set; } = null;
 
         [Newtonsoft.Json.JsonProperty("solution_id", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int? SolutionID { get; set; } = null;
+        public int SolutionID { get; set; }
 
         [Newtonsoft.Json.JsonProperty("datastore_id", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int? DatastoreID { get; set; } = null;
+        public int DatastoreID { get; set; }
 
         [Newtonsoft.Json.JsonProperty("site_id", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int? SiteID { get; set; } = null;
+        public int SiteID { get; set; }
 
         [Newtonsoft.Json.JsonProperty("network_id", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int? NetworkID { get; set; } = null;
+        public int NetworkID { get; set; }
 
         [Newtonsoft.Json.JsonProperty("external_ip_required", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public bool? ExternalIPRequired { get; set; }
+        public bool ExternalIPRequired { get; set; }
 
         [Newtonsoft.Json.JsonProperty("ssh_keys", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public IEnumerable<string> SSHKeys { get; set; } = null;
 
         [Newtonsoft.Json.JsonProperty("encrypt", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public bool? Encrypt { get; set; } = null;
+        public bool Encrypt { get; set; }
     }
 
     public class CreateVirtualMachineRequestParameter
