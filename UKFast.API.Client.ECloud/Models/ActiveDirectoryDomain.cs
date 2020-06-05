98a9b367d57388e6b15ef14e4b31f8c7c92c2bfc
Lee Farrell <lee.farrell@ukfast.co.uk>
2020-06-05 10:24:31 +0100

    tabs to spaces

--- UKFast.API.Client.ECloud/Models/ActiveDirectoryDomain.cs
+++ UKFast.API.Client.ECloud/Models/ActiveDirectoryDomain.cs
@@ -5,12 +5,12 @@ using UKFast.API.Client.Models;
 
 namespace UKFast.API.Client.ECloud.Models
 {
-	public class ActiveDirectoryDomain : ModelBase
-	{
-		[Newtonsoft.Json.JsonProperty("id")]
-		public int ID { get; set; }
+    public class ActiveDirectoryDomain : ModelBase
+    {
+        [Newtonsoft.Json.JsonProperty("id")]
+        public int ID { get; set; }
 
-		[Newtonsoft.Json.JsonProperty("name")]
-		public string Name { get; set; }
-	}
+        [Newtonsoft.Json.JsonProperty("name")]
+        public string Name { get; set; }
+    }
 }
