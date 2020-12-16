9414329cb22a7221497dfdc63b154a187a3d45f2
Lee Farrell <lee.farrell@ukfast.co.uk>
2020-12-16 12:12:37 +0000

    add more instance operations

---
+++ UKFast.API.Client.ECloud/Models/V2/Credential.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using UKFast.API.Client.Json;
+using UKFast.API.Client.Models;
+
+namespace UKFast.API.Client.ECloud.Models.V2
+{
+    public class Credential : ModelBase
+    {
+        [JsonProperty("id")]
+        public string ID { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("resource_id")]
+        public string ResourceID { get; set; }
+
+        [JsonProperty("host")]
+        public string Host { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+        
+        [JsonProperty("port")]
+        public int Port { get; set; }
+
+        [JsonProperty("created_at")]
+        [JsonConverter(typeof(DateTimeConverter))]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        [JsonConverter(typeof(DateTimeConverter))]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
